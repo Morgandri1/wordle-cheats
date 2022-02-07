5a1a3149a78bd33a03789b5558b967b1d23810b0
Morgan Metz <68765538+Morgandri1@users.noreply.github.com>
2022-02-07 00:55:05 -0500

    Add files via upload

---
+++ wordle-solver.py
@@ -0,0 +1,19 @@
+letter = input('enter letter seperated by ' ' spaces: ')
+
+letter_list = letter.split(' ')
+
+with open("words.txt", "r") as filea:
+    
+    lines = filea.readlines()
+    
+    for word in lines:
+        satisfy = True
+        
+        for letter in letter_list:
+            
+            if letter not in word:
+
+                satisfy = False
+
+        if satisfy:
+            print(word)
